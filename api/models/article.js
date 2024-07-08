dfc8bf6888dda76bf4c198cca188b7efdb03fb96
arionkosturi <arionkosturi@gmail.com>
2024-07-08 18:46:22 +0200

    added isPublished

--- api/models/article.js
+++ api/models/article.js
@@ -1,20 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const articleSchema = mongoose.Schema({
-_id: mongoose.Schema.Types.ObjectId,
-title: String,
-description: String,
-imgUrl: String,
-img2Url: String,
-content: String,
-content2: String,
-content3: String,
-category: String,
-sourceUrl: String,
-author: String,
-createdAt: Date
-},
-{ timestamps: true }
+const articleSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    title: String,
+    description: String,
+    imgUrl: String,
+    img2Url: String,
+    content: String,
+    content2: String,
+    content3: String,
+    category: String,
+    sourceUrl: String,
+    author: String,
+    isPublished: Boolean,
+    updatedAt: Date,
+    createdAt: Date,
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model('Article', articleSchema);
+module.exports = mongoose.model("Article", articleSchema);
